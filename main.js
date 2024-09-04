08d28867feea3a8932512f95395985921f37985e
Matt <hurley04155@gmail.com>
2024-09-03 22:13:53 -0400

    Fix playRound bug

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
+
 function getComputerChoice() {
     const computerChoices = ["Rock", "Paper", "Scissors"];
     const computerAnswer = Math.floor(Math.random()*computerChoices.length);
-    if (computerAnswer == 0) {
+    if (computerAnswer === 0) {
         return "Rock";
-    } else if (computerAnswer == 1) {
+    } else if (computerAnswer === 1) {
         return "Paper";
     } else {
         return "Scissors";
@@ -22,6 +27,39 @@ function getHumanChoice() {
         console.log("That is not a valid answer");
     }
 }
+
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log('You win!');
+        // return humanScore += 1; 
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        console.log('You lose!');
+        // return computerScore += 1;
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log('You win!');
+        // return humanScore += 1;
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        console.log('You lose!');
+        // return computerScore += 1;
+    } else if (humanChoice == "Rock" && computerChoice === "Scissors") {
+        console.log('You win!');
+        // return humanScore += 1;
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        console.log('You lose!');
+        // return computerScore += 1;
+    } else {
+        console.log("It's a tie!");
+    }
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
 
 
 
