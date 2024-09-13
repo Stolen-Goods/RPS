68634605b8937c853c8b1c7f0265f080cf34befc
Matt <hurley04155@gmail.com>
2024-09-12 20:09:39 -0400

    Create stylesheet

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("rock");
+
+
+
 function getComputerChoice() {
     const computerChoices = ["Rock", "Paper", "Scissors"];
     const computerAnswer = Math.floor(Math.random()*computerChoices.length);
@@ -10,24 +16,24 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    const humanChoices = prompt("Rock, Paper, or Scissors").toLowerCase();
-    if (humanChoices === "rock") {
-        return "Rock";
-    } else if (humanChoices === "paper") {
-        return "Paper";
-    } else if (humanChoices === "scissors") {
-        return "Scissors";
-    } else {
-        return "That is not a valid answer";
-    }
-}
+// function getHumanChoice() {
+//     const humanChoices = prompt("Rock, Paper, or Scissors").toLowerCase();
+//     if (humanChoices === "rock") {
+//         return "Rock";
+//     } else if (humanChoices === "paper") {
+//         return "Paper";
+//     } else if (humanChoices === "scissors") {
+//         return "Scissors";
+//     } else {
+//         return "That is not a valid answer";
+//     }
+// }
 
-function playGame() {
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    let humanScore = 0;
-    let computerScore = 0;
+// function playGame() {
+//     let humanSelection = getHumanChoice();
+//     let computerSelection = getComputerChoice();
+//     let humanScore = 0;
+//     let computerScore = 0;
     function playRound(humanChoice, computerChoice) {
         if (humanChoice === computerChoice) {
             return "It's a tie!";
@@ -44,24 +50,19 @@ function playGame() {
             computerScore++;
             return 'You lose!';
         }
-    }
-    for (let i = 0; i < 5; i++) {
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
-        console.log(humanScore);
-        console.log(computerScore);
-        console.log(playRound(humanSelection, computerSelection));
-    }
-    if (humanScore > computerScore) {
-        console.log("You are the winner!");
-    } else if (humanScore < computerScore) {
-        console.log("The computer is the winner!");
-    } else {
-        console.log("You tied with the computer!");
-    }
-}
 
-playGame();
+        rock.addEventListener("click", playRound(rock, getComputerChoice()));
+//     }
+//     if (humanScore > computerScore) {
+//         console.log("You are the winner!");
+//     } else if (humanScore < computerScore) {
+//         console.log("The computer is the winner!");
+//     } else {
+//         console.log("You tied with the computer!");
+//     }
+// }
+
+// playGame();
 
 
 
