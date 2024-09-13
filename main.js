34625e457e5297e591475f9d6e7790cd43787394
Matt <hurley04155@gmail.com>
2024-09-12 22:04:11 -0400

    Work on main interactivity with JS

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
-const scissors = document.getElementById("rock");
-
-
+const scissors = document.getElementById("scissors");
+const human = document.getElementById("human-score");
+const computer = document.getElementById("computer-score");
+const winning = document.getElementById("winning");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const computerChoices = ["Rock", "Paper", "Scissors"];
@@ -10,64 +13,53 @@ function getComputerChoice() {
     if (computerAnswer === 0) {
         return "Rock";
     } else if (computerAnswer === 1) {
-        return "Paper";
+        return "Paper"; 
     } else {
         return "Scissors";
     }
 }
 
-// function getHumanChoice() {
-//     const humanChoices = prompt("Rock, Paper, or Scissors").toLowerCase();
-//     if (humanChoices === "rock") {
-//         return "Rock";
-//     } else if (humanChoices === "paper") {
-//         return "Paper";
-//     } else if (humanChoices === "scissors") {
-//         return "Scissors";
-//     } else {
-//         return "That is not a valid answer";
-//     }
-// }
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        humanScore++;
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        humanScore++;
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        humanScore++;
+    } else {
+        computerScore++;
+    }
+}    
 
-// function playGame() {
-//     let humanSelection = getHumanChoice();
-//     let computerSelection = getComputerChoice();
-//     let humanScore = 0;
-//     let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            return "It's a tie!";
-        } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-            humanScore++;
-            return 'You win!';
-        } else if (humanChoice === "Paper" && computerChoice === "Rock") {
-            humanScore++;
-            return 'You win!';
-        } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-            humanScore++;
-            return 'You win!';
-        } else {
-            computerScore++;
-            return 'You lose!';
-        }
-
-        rock.addEventListener("click", playRound(rock, getComputerChoice()));
-//     }
-//     if (humanScore > computerScore) {
-//         console.log("You are the winner!");
-//     } else if (humanScore < computerScore) {
-//         console.log("The computer is the winner!");
-//     } else {
-//         console.log("You tied with the computer!");
-//     }
-// }
-
-// playGame();
-
-
-
-
-
-
-
-
+rock.addEventListener("click", () => {
+    playRound("Rock", getComputerChoice());
+    human.textContent = `Your score is: ${humanScore}`;
+    computer.textContent = `The computer score is: ${computerScore}`;
+    if (humanScore >= 5 && computerScore < 5) {
+        winning.textContent = "You are the winner!";
+    } else if (humanScore < 5 && computerScore >= 5) {
+        winning.textContent = "The computer beats you!";
+    }
+});
+paper.addEventListener("click", () => {
+    playRound("Paper", getComputerChoice());
+    human.textContent = `Your score is: ${humanScore}`;
+    computer.textContent = `The computer score is: ${computerScore}`;
+    if (humanScore >= 5 && computerScore < 5) {
+        winning.textContent = "You are the winner!";
+    } else if (humanScore < 5 && computerScore >= 5) {
+        winning.textContent = "The computer beats you!";
+    }
+});
+scissors.addEventListener("click", () => {
+    playRound("Scissors", getComputerChoice());
+    human.textContent = `Your score is: ${humanScore}`;
+    computer.textContent = `The computer score is: ${computerScore}`;
+    if (humanScore >= 5 && computerScore < 5) {
+        winning.textContent = "You are the winner!";
+    } else if (humanScore < 5 && computerScore >= 5) {
+        winning.textContent = "The computer beats you!";
+    }
+});
